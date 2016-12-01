0efdd2d08ff282b23e42d42ec9e7782f02200839
yinyuxia <gzbjyinyuxia@163.com>
2016-12-01 10:39:09 +0800

    spring

--- src/main/java/com/yin/controller/TestController.java
+++ src/main/java/com/yin/controller/TestController.java
@@ -1,8 +1,19 @@
 package com.yin.controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by yinyuxia on 2016/11/25.
  */
-public class TestController
-{
+public class TestController extends HttpServlet {
+    public static void mian(String[] args){
+        HttpServletRequest request=null;
+        Cookie[] cookies= request.getCookies();
+        for(Cookie c:cookies){
+
+        }
+    }
+
 }
